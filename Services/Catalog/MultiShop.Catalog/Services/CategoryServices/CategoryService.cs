5b0c2facf5f70ea43256c926aa50214363b056cd
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-13 14:54:58 +0300

    Category constructior oluşturulması

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.DTOs.CategoryDTOs;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var mongoClient = new MongoClient(_databaseSettings.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(_databaseSettings.DatabaseName);
+            _categoryCollection = mongoDatabase.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDTO>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIDCategoryDTO> GetByIDCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
