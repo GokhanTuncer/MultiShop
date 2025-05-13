a79baf27525827c1d05783a9c3531476ff57ac42
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-13 14:08:20 +0300

    Category ve Product DTOları eklenmesi

---
+++ Services/Catalog/MultiShop.Catalog/DTOs/ProductDTOs/CreateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Catalog.DTOs.ProductDTOs
+{
+    public class CreateProductDTO
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImageURL { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryID { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/DTOs/ProductDTOs/UpdateProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.DTOs.ProductDTOs
+{
+    public class UpdateProductDTO
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImageURL { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryID { get; set; }
+    }
+}
