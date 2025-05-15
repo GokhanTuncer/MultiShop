2427960007d6a91ae328197758d82f732513cff9
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-15 12:50:27 +0300

    Discount Coupon Controller eklendi

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.DTOs;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CouponList()
+        {
+            var result = await _discountService.GetAllCouponAsync();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCouponByID(int id)
+        {
+            var result = await _discountService.GetByIDCouponAsync(id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCoupon(CreateCouponDTO createCouponDTO)
+        {
+            await _discountService.CreateCouponAsync(createCouponDTO);
+            return Ok("Kupon başarıyla oluşturuldu");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCoupon(int id)
+        {
+            await _discountService.DeleteCouponAsync(id);
+            return Ok("Kupon başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCoupon(UpdateCouponDTO updateCouponDTO)
+        {
+            await _discountService.UpdateCouponAsync(updateCouponDTO);
+            return Ok("Kupon başarıyla güncellendi");
+        }
+    }
+}
