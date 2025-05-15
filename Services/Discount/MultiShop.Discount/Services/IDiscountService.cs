b2a4fa501a85adb93332e344ae366f55b34d49e1
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-15 12:16:58 +0300

    DTOlar ve servislerin eklenmesi

---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Discount.DTOs;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultCouponDTO>> GetAllCouponAsync();
+        Task CreateCouponAsync(CreateCouponDTO createCouponDTO);
+        Task UpdateCouponAsync(UpdateCouponDTO updateCouponDTO);
+        Task DeleteCouponAsync(int id);
+        Task<GetByIDCouponDTO> GetByIDCouponAsync(int id);
+
+
+    }
+}
