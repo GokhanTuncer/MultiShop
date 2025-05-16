da791a148685f8f47217148b1e4ce27f1a77482e
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-16 16:29:20 +0300

    Mediator için Ordering işlemler eklendi

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIDQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIDQuery : IRequest<GetOrderingByIDQueryResult> //Tek bir değer döndürecek
+    {
+        public int ID { get; set; }
+        public GetOrderingByIDQuery(int id)
+        {
+            ID = id;
+        }
+    }
+}
