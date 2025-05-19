a115bf0afa386b2ac81e03fc2e58d83a032fa0bc
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-19 13:32:08 +0300

    Kullanıcı register controller ve dtosu eklendi

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.DTOs;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDTO userRegisterDTO)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDTO.Username,
+                Email = userRegisterDTO.Email,
+                Name = userRegisterDTO.Name,
+                Surname = userRegisterDTO.Surname
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDTO.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla eklendi");
+            }
+            else
+            {
+                // Handle errors
+                return Ok("Bir hata oluştu");
+            }
+        }
+
+    }
+}
