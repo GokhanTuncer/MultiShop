d0aa598847f70d56801982f9599b409a63bd0243
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-24 15:30:50 +0300

    Sepet Servisinin eklenmesi

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Basket.DTOs;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            await _redisService.GetDB().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDTO> GetBasket(string userId)
+        {
+           var existBasket = await _redisService.GetDB().StringGetAsync(userId);
+            return JsonSerializer.Deserialize<BasketTotalDTO>(existBasket);
+                }
+
+        public async Task SaveBasket(BasketTotalDTO basketTotalDTO)
+        {
+            await _redisService.GetDB().StringSetAsync(basketTotalDTO.UserID, JsonSerializer.Serialize(basketTotalDTO));
+        }
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Basket.DTOs;
+
+namespace MultiShop.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDTO> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDTO basketTotalDTO);
+        Task DeleteBasket(string userId);
+    }
+}
