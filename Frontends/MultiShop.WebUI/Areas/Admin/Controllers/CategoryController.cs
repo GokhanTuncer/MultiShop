dff29bdf3bcd5cbf9075284e4a204ba01330363d
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-29 15:24:55 +0300

    Kategori ekleme sayfası

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
 using Newtonsoft.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpclientFactory;
@@ -16,7 +18,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             _httpclientFactory = httpclientFactory;
         }
 
-        [AllowAnonymous]
+        
         public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -33,6 +35,30 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 return View(values);
             }
 
+            return View();
+        }
+
+        [HttpGet]
+        public  IActionResult CreateCategory()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategori Yönetimi";
+            ViewBag.v3 = "Kategori Ekle";
+            ViewBag.v0 = "Kategori İşlemleri";
+            return View();
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDTO createCategoryDTO)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDTO);
+            var content = new StringContent(jsonData,Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44312/api/Categories", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Category", new {area="Admin"});
+            }
             return View();
         }
     }
