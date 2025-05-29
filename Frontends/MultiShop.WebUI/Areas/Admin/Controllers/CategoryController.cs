599b0ef0971ff351b02fb388aeabb6f27a8492e5
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-29 14:57:10 +0300

    Category dtonun eklenmesi, controllerinin consume edilmesi, ek resimlerin eklenmesi

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public CategoryController(IHttpClientFactory httpclientFactory)
+        {
+            _httpclientFactory = httpclientFactory;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
             ViewBag.v2 = "Kategori Yönetimi";
             ViewBag.v3 = "Kategori Listesi";
             ViewBag.v0 = "Kategori İşlemleri";
+
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
