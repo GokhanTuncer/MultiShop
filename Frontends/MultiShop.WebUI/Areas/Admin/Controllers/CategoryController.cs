6dd7c59b0bf555d44cb0f9e220114b8fa29101d4
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-29 15:49:36 +0300

    Kategori oluşturma ve silme

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -9,6 +9,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [AllowAnonymous]
+    [Route("Admin/Category")]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpclientFactory;
@@ -18,7 +19,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             _httpclientFactory = httpclientFactory;
         }
 
-        
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -39,25 +40,39 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public  IActionResult CreateCategory()
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
         {
             ViewBag.v1 = "Ana Sayfa";
             ViewBag.v2 = "Kategori Yönetimi";
             ViewBag.v3 = "Kategori Ekle";
             ViewBag.v0 = "Kategori İşlemleri";
             return View();
-        } 
+        }
 
         [HttpPost]
+        [Route("CreateCategory")]
         public async Task<IActionResult> CreateCategory(CreateCategoryDTO createCategoryDTO)
         {
             var client = _httpclientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(createCategoryDTO);
-            var content = new StringContent(jsonData,Encoding.UTF8, "application/json");
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PostAsync("https://localhost:44312/api/Categories", content);
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index","Category", new {area="Admin"});
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpDelete]
+        [Route("DeleteCategory/{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44312/api/Categories/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
             }
             return View();
         }
