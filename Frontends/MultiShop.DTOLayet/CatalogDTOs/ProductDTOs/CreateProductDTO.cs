df57baa0661eceb279544e1f9cb3deb060836777
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-05-30 15:56:34 +0300

    Ürün oluşturma

--- Frontends/MultiShop.DTOLayet/CatalogDTOs/ProductDTOs/CreateProductDTO.cs
+++ Frontends/MultiShop.DTOLayet/CatalogDTOs/ProductDTOs/CreateProductDTO.cs
@@ -8,5 +8,10 @@ namespace MultiShop.DTOLayer.CatalogDTOs.ProductDTOs
 {
     public class CreateProductDTO
     {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImageURL { get; set; }
+        public string ProductDescription { get; set; }
+        public string CategoryID { get; set; }
     }
 }
--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
 using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
@@ -35,6 +38,42 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 return View(values);
             }
 
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürün Yönetimi";
+            ViewBag.v3 = "Ürün Listesi";
+            ViewBag.v0 = "Ürün İşlemleri";
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/Categories");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+            List<SelectListItem> categories = (from c in values
+                                               select new SelectListItem
+                                               {
+                                                   Text = c.CategoryName,
+                                                   Value = c.CategoryID
+                                               }).ToList();
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(CreateProductDTO createProductDTO)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDTO);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44312/api/Products", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
             return View();
         }
     }
