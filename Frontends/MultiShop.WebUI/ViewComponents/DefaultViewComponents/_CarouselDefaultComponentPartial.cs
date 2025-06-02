a381479209dfc80e9b1d7a200046e1ab60b6070f
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-02 16:23:57 +0300

    verinin çekilmesi

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.FeatureSliderDTOs;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CarouselDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpclientFactory;
+        public _CarouselDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpclientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/FeatureSliders");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
