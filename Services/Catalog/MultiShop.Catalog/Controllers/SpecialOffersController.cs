5ec3d55a6d9fe1f5e913693aecf9fd974eb3b745
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-03 15:49:53 +0300

    Special Offer api ve program kaydı

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.SpecialOfferDTOs;
+using MultiShop.Catalog.Services.SpecialOfferServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var values = await _specialOfferService.GetAllSpecialOfferAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialOfferByID(string id)
+        {
+            var values = await _specialOfferService.GetByIDSpecialOfferAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDTO createSpecialOfferDTO)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDTO);
+            return Ok("Özel Fırsat başarıyla eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDTO updateSpecialOfferDTO)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDTO);
+            return Ok("Özel Fırsat başarıyla güncellendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id);
+            return Ok("Özel Fırsat başarıyla silindi");
+        }
+
+    }
+}
