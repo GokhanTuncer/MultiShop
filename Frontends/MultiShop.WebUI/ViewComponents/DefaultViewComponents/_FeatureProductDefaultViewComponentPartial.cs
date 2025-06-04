7acd2c22b4f691da273c850d7d83334a024a30ef
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-04 15:14:47 +0300

    Öne Çıkan Ürünler bölümünün dinamik hale getirilmesi

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductDefaultViewComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductDefaultViewComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductDefaultViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public _FeatureProductDefaultViewComponentPartial(IHttpClientFactory httpclientFactory)
         {
+            _httpclientFactory = httpclientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
