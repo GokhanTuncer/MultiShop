75edc0da472d7976cebe08dd72f3d7c2bd84bd4d
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-09 12:45:28 +0300

    offer discount api controlleri eklenmesi

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/OfferDiscountsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.OfferDiscountDTOs;
+using MultiShop.Catalog.Services.OfferDiscountServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class OfferDiscountController : ControllerBase
+    {
+        private readonly IOfferDiscountService _offerDiscountService;
+
+        public OfferDiscountController(IOfferDiscountService OfferDiscountService)
+        {
+            _offerDiscountService = OfferDiscountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OfferDiscountList()
+        {
+            var values = await _offerDiscountService.GetAllOfferDiscountAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOfferDiscountByID(string id)
+        {
+            var values = await _offerDiscountService.GetByIDOfferDiscountAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOfferDiscount(CreateOfferDiscountDTO createOfferDiscountDTO)
+        {
+            await _offerDiscountService.CreateOfferDiscountAsync(createOfferDiscountDTO);
+            return Ok("Indirim teklifi başarıyla eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOfferDiscount(UpdateOfferDiscountDTO updateOfferDiscountDTO)
+        {
+            await _offerDiscountService.UpdateOfferDiscountAsync(updateOfferDiscountDTO);
+            return Ok("Indirim teklifi başarıyla güncellendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOfferDiscount(string id)
+        {
+            await _offerDiscountService.DeleteOfferDiscountAsync(id);
+            return Ok("Indirim teklifi başarıyla silindi");
+        }
+    }
+}
