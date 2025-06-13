0397493dbb1a7e00de24a30f2073f4223329d175
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-13 12:32:07 +0300

    Ürüne ait detayları listeleme

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailFeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public _ProductDetailFeatureComponentPartial(IHttpClientFactory httpclientFactory)
         {
+            _httpclientFactory = httpclientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/Products/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<UpdateProductDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
