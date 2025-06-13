1daa9a32e20c65e7767b8ea4be8ae0636ef65778
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-13 12:15:51 +0300

    Kategoriye göre veri getirme

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _NavbarUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public _NavbarUILayoutComponentPartial(IHttpClientFactory httpclientFactory)
         {
+            _httpclientFactory = httpclientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
