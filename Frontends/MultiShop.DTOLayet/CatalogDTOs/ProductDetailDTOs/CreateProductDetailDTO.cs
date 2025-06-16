e8f7d43f25282481fbb2161f4296db44b5ac2f0c
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-16 12:26:20 +0300

    Ürün bilgisi güncelleme dto, controller ve view'i

---
+++ Frontends/MultiShop.DTOLayet/CatalogDTOs/ProductDetailDTOs/CreateProductDetailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.DTOLayer.CatalogDTOs.ProductDetailDTOs
+{
+    public class CreateProductDetailDTO
+    {
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        public string ProductID { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ProductDetailDTOs;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/ProductDetail")]
+    public class ProductDetailController : Controller
+    {
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public ProductDetailController(IHttpClientFactory httpclientFactory)
+        {
+            _httpclientFactory = httpclientFactory;
+        }
+        [Route("UpdateProductDetail/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateProductDetail(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürün Detayı Yönetimi";
+            ViewBag.v3 = "Ürün Detayı Güncelle";
+            ViewBag.v0 = "Ürün Detayı İşlemleri";
+            var client = _httpclientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44312/api/ProductDetails/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDetailDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("UpdateProductDetail/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDTO updateProductDetailDTO)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDetailDTO);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44312/api/ProductDetails", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "ProductDetail", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
