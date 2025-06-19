89ef21c5bbd9547c4047a68693c6801a76c6afbe
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-19 13:44:07 +0300

    Contact'ın önyüze eklenmesi

--- Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CatalogDTOs.ContactDTOs;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public ContactController(IHttpClientFactory httpclientFactory)
+        {
+            _httpclientFactory = httpclientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDTO createContactDTO)
+        {
+            var client = _httpclientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDTO);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44312/api/Contacts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Contact", new { area = "Admin" });
+            }
             return View();
         }
     }
+    
 }
+
+
