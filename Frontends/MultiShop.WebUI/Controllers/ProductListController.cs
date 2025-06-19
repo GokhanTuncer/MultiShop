a30e12742c4c967301df1811196bce60554e1857
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-19 13:04:15 +0300

    controller düzenlemesi

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.CommentDTOs;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -14,10 +23,25 @@ namespace MultiShop.WebUI.Controllers
             ViewBag.x = id;
             return View();
         }
-        public PartialViewResult AddComment()
+
+        [HttpGet]
+        public async Task<PartialViewResult> AddComment(string id)
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7126/api/Comments/CommentListByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDTO>>(jsonData);
+                return PartialView(values);
+            }
+
             return PartialView();
         }
-
+        [HttpPost]
+        public IActionResult AddComment(CreateCommentDTO createCommentDTO)
+        {
+            return RedirectToAction("Index", "Default");
+        }
     }
 }
