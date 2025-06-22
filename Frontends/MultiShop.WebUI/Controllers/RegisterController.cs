546845d9f75cd8c0a2ff6d353fe98b24fac936e1
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-22 14:09:07 +0300

    Register kontrolleri ve sayfası düzenlemesi 2

--- Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
+++ Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.IdentityDTOs.RegisterDTOs;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly IHttpClientFactory _httpclientFactory;
+
+        public RegisterController(IHttpClientFactory httpclientFactory)
+        {
+            _httpclientFactory = httpclientFactory;
+        }
         [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDTO createRegisterDTO)
+        {
+            if (createRegisterDTO.Password == createRegisterDTO.ConfirmPassword)
+            {
+                var client = _httpclientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createRegisterDTO);
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("http://localhost:5001/api/Registers", content);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            
             return View();
         }
     }
