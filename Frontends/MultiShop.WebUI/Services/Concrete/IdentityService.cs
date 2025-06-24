7d1b19f88a6075c792ca81bbd97b1af3cd627f32
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-24 16:28:45 +0300

    Client setting sınıfı

---
+++ Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,25 @@
+﻿using MultiShop.DTOLayer.IdentityDTOs.LoginDTOs;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Settings;
+
+namespace MultiShop.WebUI.Services.Concrete
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ClientSettings _clientSettings;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public IdentityService(HttpClient httpClient, ClientSettings clientSettings, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClient;
+            _clientSettings = clientSettings;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task<bool> SignIn(SignUpDTO signUpDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
