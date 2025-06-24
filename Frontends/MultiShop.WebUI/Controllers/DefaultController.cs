c656d3725547be07d9169e7664868a0bd24456b1
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-24 13:24:46 +0300

    Token altyapısının oluşturulması

--- Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
+++ Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
@@ -5,7 +5,7 @@ namespace MultiShop.WebUI.Controllers
     public class DefaultController : Controller
     {
         public IActionResult Index()
-        {
+        {      
             return View();
         }
     }
---
+++ IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MultiShop.IdentityServer.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "http://localhost";
+        public const string ValidIssuer = "http://localhost";
+        public const string Key = "MultiShop..010203040506070809TestKey*-=+_)(*&^%$#@!~`";
+        public const int Expire = 60; 
+    }
+}
