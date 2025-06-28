1aa9e67f4af39ec6fee12f8c41b52a7e43e83efe
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-06-28 12:56:30 +0300

    UIde Category Servisi

---
+++ Frontends/MultiShop.DTOLayet/CatalogDTOs/CategoryDTOs/GetByIDCategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs
+{
+    public class GetByIDCategoryDTO
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/CatalogServices/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DTOLayer.CatalogDTOs.CategoryDTOs;
+
+namespace MultiShop.WebUI.Services.CatalogServices.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDTO>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDTO createCategoryDTO);
+        Task UpdateCategoryAsync(UpdateCategoryDTO updateCategoryDTO);
+        Task DeleteCategoryAsync(string id);
+        Task<UpdateCategoryDTO> GetByIDCategoryAsync(string id);
+    }
+}
