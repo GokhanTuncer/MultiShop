355872edb8094a8cd8b3ec24f924dbad5a40ebcb
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-02 14:13:15 +0300

    url dirctory layouta taşındı

--- Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
+++ Frontends/MultiShop.WebUI/Controllers/DefaultController.cs
@@ -5,7 +5,10 @@ namespace MultiShop.WebUI.Controllers
     public class DefaultController : Controller
     {
         public IActionResult Index()
-        {      
+        {
+            ViewBag.directory1 = "Multishop";
+            ViewBag.directory2 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürün Listesi";
             return View();
         }
     }
