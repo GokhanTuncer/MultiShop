8b2b09e29fab13f8b4869c37eddfa6126bca3a47
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-06 19:32:35 +0300

    OrderAddress servisi ve sınıfı

---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/OrderAddressServices/IOrderAddressService.cs
@@ -0,0 +1,9 @@
+﻿using MultiShop.DTOLayer.OrderDTOs.OrderAddressDTOs;
+
+namespace MultiShop.WebUI.Services.OrderServices.OrderAddressServices
+{
+    public interface IOrderAddressService
+    {
+        Task CreateOrderAddressAsync(CreateOrderAddressDTO createOrderAddressDto);
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/OrderAddressServices/OrderAddressService.cs
@@ -0,0 +1,17 @@
+﻿using MultiShop.DTOLayer.OrderDTOs.OrderAddressDTOs;
+
+namespace MultiShop.WebUI.Services.OrderServices.OrderAddressServices
+{
+    public class OrderAddressService : IOrderAddressService
+    {
+        private readonly HttpClient _httpClient;
+        public OrderAddressService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task CreateOrderAddressAsync(CreateOrderAddressDTO createOrderAddressDto)
+        { 
+            await _httpClient.PostAsJsonAsync<CreateOrderAddressDTO>("addresses", createOrderAddressDto);
+        }
+    }
+}
