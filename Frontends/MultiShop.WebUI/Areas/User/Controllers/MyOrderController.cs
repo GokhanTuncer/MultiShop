6d04ec82e807dd509f06321bbb2144d1547f417d
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-07 15:15:35 +0300

    Kullanıcı alanı ve controllerlerin eklenmesi

---
+++ Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.User.Controllers
+{
+    public class MyOrderController : Controller
+    {
+        [Area("User")]
+
+        public IActionResult MyOrderList()
+        {
+            return View();
+        }
+    }
+}
