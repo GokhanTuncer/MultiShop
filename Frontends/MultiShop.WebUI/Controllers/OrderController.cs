f130a5b0ba0e096121b0d19138431fc099218661
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-07 13:26:05 +0300

    Order controller düzenlemesi

--- Frontends/MultiShop.WebUI/Controllers/OrderController.cs
+++ Frontends/MultiShop.WebUI/Controllers/OrderController.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.OrderDTOs.OrderAddressDTOs;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Services.OrderServices.OrderAddressServices;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderAddressService _orderAddressService;
+        private readonly IUserService _userService;
+
+        public OrderController(IOrderAddressService orderAddressService, IUserService userService)
+        {
+            _orderAddressService = orderAddressService;
+            _userService = userService;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.directory1 = "MultiShop";
             ViewBag.directory2 = "Siparişler";
             ViewBag.directory3 = "Sipariş İşlemleri";
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateOrderAddressDTO createOrderAddressDTO)
+        {
+            var values = await _userService.GetUserInfo();
+            createOrderAddressDTO.UserID = values.ID;
+            createOrderAddressDTO.Description = "test";
+
+            await _orderAddressService.CreateOrderAddressAsync(createOrderAddressDTO);
+            return RedirectToAction("Index", "Payment");
         }
     }
 }
