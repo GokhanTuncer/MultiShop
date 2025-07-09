d11e79d534d104d013ab7e648037bfb1b85b1c08
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-09 13:14:39 +0300

    OrderOrderingService

---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/OrderOrderingServices/IOrderOrderingService.cs
@@ -0,0 +1,9 @@
+﻿using MultiShop.DTOLayer.OrderDTOs.OrderOrderingDTOs;
+
+namespace MultiShop.WebUI.Services.OrderServices.OrderOrderingServices
+{
+    public interface IOrderOrderingService
+    {
+        Task<List<ResultOrderingByUserIDDTO>> GetOrderingByUserID(string id);
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/OrderOrderingServices/OrderOrderingService.cs
@@ -0,0 +1,22 @@
+﻿using MultiShop.DTOLayer.OrderDTOs.OrderOrderingDTOs;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Services.OrderServices.OrderOrderingServices
+{
+    public class OrderOrderingService : IOrderOrderingService
+    {
+        private readonly HttpClient _httpClient;
+        public OrderOrderingService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<ResultOrderingByUserIDDTO>> GetOrderingByUserID(string id)
+        {
+            
+            var responseMessage = await _httpClient.GetAsync($"orderings/GetOrderingByUserId/{id}");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultOrderingByUserIDDTO>>(jsonData);
+            return values;
+        }
+    }
+}
