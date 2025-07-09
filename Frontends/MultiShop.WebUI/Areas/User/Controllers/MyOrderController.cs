37d1e609606214f859403ad7fa095cd8519000d5
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-09 13:29:34 +0300

    MyOrderController, kullanıcıya göre sipariş geçmişi

--- Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
+++ Frontends/MultiShop.WebUI/Areas/User/Controllers/MyOrderController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Services.OrderServices.OrderOrderingServices;
 
 namespace MultiShop.WebUI.Areas.User.Controllers
 {
+    [Area("User")]
     public class MyOrderController : Controller
     {
-        [Area("User")]
+        private readonly IOrderOrderingService _orderOrderingService;
+        private readonly IUserService _userService;
 
-        public IActionResult MyOrderList()
+        public MyOrderController(IUserService userService, IOrderOrderingService orderOrderingService)
         {
+            _userService = userService;
+            _orderOrderingService = orderOrderingService;
+        }
+
+        public async Task<IActionResult> MyOrderList()
+        {
+            var user = await _userService.GetUserInfo();
+            var values = await _orderOrderingService.GetOrderingByUserID(user.ID);
             return View();
         }
     }
