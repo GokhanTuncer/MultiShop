02ad7c13bb3f452d4250029dbb2a108f12ba0d2e
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-16 13:09:00 +0300

    Gelen/Giden kutusu düzenlemesi

--- Frontends/MultiShop.WebUI/Areas/User/Controllers/MessageController.cs
+++ Frontends/MultiShop.WebUI/Areas/User/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.Interfaces;
 using MultiShop.WebUI.Services.MessageServices;
 
 namespace MultiShop.WebUI.Areas.User.Controllers
@@ -7,15 +8,25 @@ namespace MultiShop.WebUI.Areas.User.Controllers
     public class MessageController : Controller
     {
         private readonly IMessageService _messageService;
+        private readonly IUserService _userService;
 
-        public MessageController(IMessageService messageService)
+        public MessageController(IMessageService messageService, IUserService userService)
         {
             _messageService = messageService;
+            _userService = userService;
         }
 
         public async Task<IActionResult> Inbox(string id)
         {
-            return View();
+            var user = await _userService.GetUserInfo();
+            var values = await _messageService.GetInboxMessageAsync(user.ID);
+            return View(values);
+        }
+        public async Task<IActionResult> Sendbox(string id)
+        {
+            var user = await _userService.GetUserInfo();
+            var values = await _messageService.GetSendboxMessageAsync(user.ID);
+            return View(values);
         }
     }
 }
