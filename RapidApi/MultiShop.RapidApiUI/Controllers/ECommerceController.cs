1a835d3f4756b6af522090ff523f0924d8becd71
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-23 16:20:24 +0300

    Rapidapi ecommerce test list

---
+++ RapidApi/MultiShop.RapidApiUI/Controllers/ECommerceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiUI.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MultiShop.RapidApiUI.Controllers
+{
+    public class ECommerceController : Controller
+    {
+        public async Task<IActionResult> ECommerceList()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-product-search.p.rapidapi.com/search-light-v2?q=logitech%20fare&country=tr&language=en&page=1&limit=10&sort_by=BEST_MATCH&product_condition=ANY&return_filters=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "59da85055bmshdb0ee20395bbf7cp129fedjsnc26e8d8bf355" },
+        { "x-rapidapi-host", "real-time-product-search.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ECommerceViewModel.Rootobject>(body);
+                return View(values.data.products.ToList());
+            }
+        }
+    }
+}
