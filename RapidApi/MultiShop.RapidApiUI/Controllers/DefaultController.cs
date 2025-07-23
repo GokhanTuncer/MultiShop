5b00a98e857577f60b7985b5ae0c16618668f025
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-07-23 14:38:02 +0300

    rapidapi Exchange test

--- RapidApi/MultiShop.RapidApiUI/Controllers/DefaultController.cs
+++ RapidApi/MultiShop.RapidApiUI/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MultiShop.RapidApiUI.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Net.Http.Headers;
 
 namespace MultiShop.RapidApiUI.Controllers
@@ -33,6 +34,28 @@ namespace MultiShop.RapidApiUI.Controllers
            
         }
 
+        public async Task<IActionResult> Exchange()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "59da85055bmshdb0ee20395bbf7cp129fedjsnc26e8d8bf355" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.exchange = values.data.exchange_rate;
+                return View(values);
+            }
+        }
 
     }
 }
